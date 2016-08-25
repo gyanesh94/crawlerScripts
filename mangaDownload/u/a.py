596d7eb891ceeccff2a1ecc0cdfcd2a35e56f229
gyanesh94 <gyanesh.cse@gmail.com>
2016-08-26 00:36:26 +0530

    integrated img2pdf to crawling

--- mangaDownload/u/a.py
+++ mangaDownload/u/a.py
@@ -1,29 +1,28 @@
-#!/usr/bin/python
+#!/Users/gyanesh/anaconda/bin/python
 
 from bs4 import BeautifulSoup
 import urllib2
 import os
-import sys
 import signal
-import getopt
+import argparse
+from imgToPdf import *
+
+
+mainDir = "/Users/gyanesh/Documents/Anime Manga/Unread"
+
+
+def argumentParser():
+    parser = argparse.ArgumentParser(description='Fetch Url')
+    parser.add_argument('-u', action="store", default=False, dest='url', help="url")
+    parser.add_argument('-p', action="store", default=False, dest='pdf', help="convert to pdf")
+    url = parser.parse_args().url
+    pdf = parser.parse_args().pdf
+    return url, pdf
 
 
 def signal_handler(signal, frame):
         print('You pressed Ctrl+C!')
         exit()
-
-
-def get_url():
-    url = ''
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "u:")
-    except getopt.GetoptError:
-        print "Command Line Error"
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-u':
-            url = arg
-    return url
 
 
 def get_src(url):
@@ -79,13 +78,14 @@ def get_image_links(url):
     return pages
 
 
-def get_images(href, name):
+def get_images(href, name, pdf):
     pages = get_image_links(href)
     name = name.replace(".", "_")
     if not os.path.isdir(name):
         os.mkdir(name)
     os.chdir(name)
     i = 0
+    print name
     for page in pages:
         tmpName = "{name} Page {index:03}.{ext}".format(name=name, index=i, ext=page[1])
         tmpName = tmpName.replace("-", " ").replace("  ", " ").replace("  ", " ")
@@ -94,10 +94,19 @@ def get_images(href, name):
         os.system(axel)
         # urllib.urlretrieve(page[0],page[1])
     os.chdir("..")
+    if pdf:
+        print "test"
+        convertToPdf("./" + name)
+    else:
+        zipCmd = "zip '{name}'.cbz -r '{name}'".format(name=name)
+        print zipCmd
+        os.system(zipCmd)
 
 signal.signal(signal.SIGINT, signal_handler)
 
-url = get_url()
+url, pdf = argumentParser()
+
+os.chdir(mainDir)
 
 if url:
     src = get_src(url)
@@ -114,15 +123,15 @@ if url:
             ch = choices[0]
             if ch == "a" or ch == "A":
                 for chapter in reversed(chapters):
-                    get_images(chapter[0], chapter[1])
+                    get_images(chapter[0], chapter[1], pdf)
             elif ch == "r" or ch == "R":
                 for i in range(int(choices[1]), int(choices[2]) + 1):
                     chapter = chapters[i]
-                    get_images(chapter[0], chapter[1])
+                    get_images(chapter[0], chapter[1], pdf)
             else:
                 choices = filter(lambda x: x.isdigit(), choices)
                 if choices:
                     choices = map(lambda x: int(x), choices)
                     for choice in choices:
                         chapter = chapters[choice]
-                        get_images(chapter[0], chapter[1])
+                        get_images(chapter[0], chapter[1], pdf)
